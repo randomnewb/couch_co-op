8e272e804f8a7338eae10bc19da85e7ae624b629
randomnewb <nguy1581@gmail.com>
2023-12-16 19:22:33 -0600

    Player/jewel overlap, score, and text

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -9,10 +9,12 @@ export default class GameScene extends Phaser.Scene {
       { up: "UP", down: "DOWN", left: "LEFT", right: "RIGHT" },
     ];
     this.keyCodes = Phaser.Input.Keyboard.KeyCodes;
+    this.scoreTexts = [];
   }
 
   preload() {
     this.load.image("player_character", "./assets/player_character.png");
+    this.load.image("jewel", "./assets/jewel.png");
   }
 
   create() {
@@ -45,8 +47,61 @@ export default class GameScene extends Phaser.Scene {
           left: this.keyCodes[this.keySets[this.players.length].left],
           right: this.keyCodes[this.keySets[this.players.length].right],
         });
+        player.score = 0;
         this.players.push(player);
+
+        const playerIndex = this.players.length;
+
+        const scoreText = this.add.text(
+          20 + 200 * (playerIndex - 1),
+          100,
+          `Player ${playerIndex}: ${player.score}`,
+          {
+            font: "25px Arial",
+            fill: "yellow",
+          }
+        );
+        this.scoreTexts.push(scoreText);
+
+        this.physics.add.overlap(
+          player,
+          this.jewel,
+          () => {
+            player.score++;
+            scoreText.setText(`Player ${playerIndex}: ${player.score}`);
+
+            this.jewel.x = Phaser.Math.Between(100, 700);
+            this.jewel.y = Phaser.Math.Between(100, 500);
+
+            console.log(`Player ${playerIndex} grabbed the jewel`);
+          },
+          null,
+          this
+        );
       }
+    });
+
+    this.jewel = this.physics.add.sprite(
+      Phaser.Math.Between(100, 700),
+      Phaser.Math.Between(100, 500),
+      "jewel"
+    );
+
+    this.players.forEach(player => {
+      player.score = 0;
+    });
+
+    this.players.forEach((player, index) => {
+      const scoreText = this.add.text(
+        20 + 200 * index,
+        100,
+        `Player ${index + 1}: ${player.score}`,
+        {
+          font: "25px Arial",
+          fill: "yellow",
+        }
+      );
+      this.scoreTexts.push(scoreText);
     });
   }
 
@@ -68,18 +123,20 @@ export default class GameScene extends Phaser.Scene {
   }
 }
 
+const SPEED = 2;
+
 const MoveLeftCommand = {
-  execute: player => (player.x -= 5),
+  execute: player => (player.x -= SPEED),
 };
 
 const MoveRightCommand = {
-  execute: player => (player.x += 5),
+  execute: player => (player.x += SPEED),
 };
 
 const MoveUpCommand = {
-  execute: player => (player.y -= 5),
+  execute: player => (player.y -= SPEED),
 };
 
 const MoveDownCommand = {
-  execute: player => (player.y += 5),
+  execute: player => (player.y += SPEED),
 };
